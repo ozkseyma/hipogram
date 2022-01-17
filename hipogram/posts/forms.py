dfd35ee3c754008f0cb2040f18796da56884fde8
seyma <ozukanarseyma@gmail.com>
2022-01-17 15:55:49 +0300

    second commit

--- hipogram/posts/forms.py
+++ hipogram/posts/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 from .models import Post
 
+#in order to create posts
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('image', 'text', 'tags',)
-        
+        fields = ('image', 'text', 'tags',)   

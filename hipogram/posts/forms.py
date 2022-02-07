071c939b400d2b9259724ec3c8a55cb8521cb4f8
seyma <ozukanarseyma@gmail.com>
2022-02-07 16:59:41 +0300

    Add tags field to Post

--- hipogram/posts/forms.py
+++ hipogram/posts/forms.py
@@ -7,4 +7,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['image', 'text', ]
+        fields = ['image', 'text', 'tags', ]
--- hipogram/posts/models.py
+++ hipogram/posts/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    tag = models.CharField(max_lenght=30)
+
+
 class Post(models.Model):
     image = models.ImageField()
     text = models.TextField()
     created_by = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     creation_datetime = models.DateTimeField(auto_now_add=True)
     # like_count = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.created_by.username

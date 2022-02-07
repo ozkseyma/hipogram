b53b514db3c440b092c15af43c7bc9da2783bbca
seyma <ozukanarseyma@gmail.com>
2022-02-07 12:02:35 +0300

    Fix PostListView's url

--- hipogram/posts/urls.py
+++ hipogram/posts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("?name", PostListView.as_view(), name="list"),
+    path("", PostListView.as_view(), name="list"),
     path('share/', views.post_new, name='post_new'),
     path('update/<int:id>/', views.update_post, name='update_post'),
     path('delete/<int:id>/', views.delete_post, name='delete_post'),

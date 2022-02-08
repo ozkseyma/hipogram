c619b1963fcf91e1147a0f384861da58980a875e
seyma <ozukanarseyma@gmail.com>
2022-02-08 12:14:23 +0300

    Make tags optional

--- hipogram/posts/models.py
+++ hipogram/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=30)
+    tag = models.CharField(max_length=30, blank=True)
 
 
 class Post(models.Model):

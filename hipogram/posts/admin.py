3b85c25ea816e8f3c65c033a22121e8053d72275
seyma <ozukanarseyma@gmail.com>
2022-02-09 14:49:16 +0300

    Improve admin panel

--- hipogram/posts/admin.py
+++ hipogram/posts/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-
 from .models import Post, Tag
 
-admin.site.register(Post)
 admin.site.register(Tag)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("created_by", "creation_datetime", "Tags")
+    list_filter = ("created_by", "tags")
+
+    def Tags(self, obj):
+        queryset = []
+        for tag in obj.tags.all():
+            queryset.append(tag)
+        return queryset
+
+
+admin.site.register(Post, PostAdmin)

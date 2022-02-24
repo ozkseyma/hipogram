297e99465b8d50d3117b79b4297443d4d27ad166
seyma <ozukanarseyma@gmail.com>
2022-02-24 12:13:38 +0300

    Make OwnerRequiredMixin more clear

--- hipogram/posts/mixins.py
+++ hipogram/posts/mixins.py
@@ -4,8 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class OwnerRequiredMixin(LoginRequiredMixin):
 
     def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
         post = self.get_object()
-        if post.created_by != self.request.user:
+        if post.created_by != request.user:
             return self.handle_no_permission()
-        return response
+        return super().dispatch(request, *args, **kwargs)

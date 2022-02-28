c4250ed08dae75f88f5acdf5c6e407c92d62e6c7
seyma <ozukanarseyma@gmail.com>
2022-02-28 15:45:11 +0300

    Add 'Change Password' & 'Edit Profile' functionalities

--- hipogram/users/urls.py
+++ hipogram/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SignUpView, LogInView, LogOutView
+from .views import SignUpView, LogInView, LogOutView, ChangePasswordView, EditProfileView
 
 app_name = "users"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LogInView.as_view(), name="login"),
     path("logout/", LogOutView.as_view(), name="logout"),
+    path("users/change/password/<int:user_id>", ChangePasswordView.as_view(), name="change_password"),
+    path("users/edit/profile/<int:user_id>", EditProfileView.as_view(), name="edit"),
 ]
--- hipogram/users/views.py
+++ hipogram/users/views.py
@@ -1,11 +1,12 @@
+# from calendar import c
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
 from django.contrib import messages
-from django.views.generic import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
 
 class SignUpView(SuccessMessageMixin, CreateView):
@@ -32,3 +33,18 @@ class LogOutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.add_message(request, messages.INFO, 'You are successfully logged out.')
         return super().dispatch(request, *args, **kwargs)
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = "change_password.html"
+    success_url = reverse_lazy("posts:list")
+    success_message = "You have successfully changed your password!"
+
+
+class EditProfileView(SuccessMessageMixin, UpdateView):
+    model = User
+    fields = ["username"]
+    template_name = "edit.html"
+    pk_url_kwarg = "user_id"
+    success_url = reverse_lazy("posts:list")
+    success_message = "You have successfully edited your profile!"

2c1775615d0b733e281c9356ae721e416560f490
seyma <ozukanarseyma@gmail.com>
2022-03-03 12:50:44 +0300

    Remove changepassword.html & change url name

--- hipogram/users/urls.py
+++ hipogram/users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LogInView.as_view(), name="login"),
     path("logout/", LogOutView.as_view(), name="logout"),
-    path("users/edit/profile/<int:user_id>", EditProfileView.as_view(), name="edit"),
+    path("users/edit-profile/<int:user_id>", EditProfileView.as_view(), name="edit"),
 ]

ce881edbfcc692685971c3888ad54ee0c7f3fd4b
seyma <ozukanarseyma@gmail.com>
2022-03-04 12:22:59 +0300

    Use decorators to register ModelAdmins

--- hipogram/posts/admin.py
+++ hipogram/posts/admin.py
@@ -3,6 +3,9 @@ from .models import Post, Tag, Like, Rate
 from django.contrib.auth.models import User
 from hipogram.core.mixins import ReadOnlyAdminMixin
 
+admin.site.register(Tag)
+admin.site.unregister(User)
+
 
 class LikeInline(admin.TabularInline):
     model = Like
@@ -16,6 +19,7 @@ class PostInline(admin.TabularInline):
     model = Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("created_by", "creation_datetime", "_tags")
     list_filter = ("created_by", "tags")
@@ -25,24 +29,19 @@ class PostAdmin(admin.ModelAdmin):
         return list(post.tags.all())
 
 
+@admin.register(Like)
 class LikeAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_filter = ("post", "user")
     list_display = ("post", "user")
 
 
+@admin.register(Rate)
 class RateAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_filter = ("post", "user")
     list_display = ("post", "user", "value")
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "email", "is_active", "is_staff")
     inlines = [PostInline]
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Like, LikeAdmin)
-admin.site.register(Rate, RateAdmin)
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-admin.site.register(Tag)
